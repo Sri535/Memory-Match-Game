987f3ef705d896744d7e40550a66a0e41039ab57
Sreenivasula Reddy M <sri535@gmail.com>
2025-04-28 17:34:05 +0530

    Update script.js
    added service worker function

--- script.js
+++ script.js
@@ -167,3 +167,13 @@ function updateHighScore() {
 function toggleTheme() {
   document.body.classList.toggle('dark-mode');
 }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker Registered!', registration);
+    })
+    .catch(function(error) {
+      console.log('Service Worker Registration Failed:', error);
+    });
+}
+
